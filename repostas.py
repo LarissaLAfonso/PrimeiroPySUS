77aa04bc4a618e1bd62e2e8f7aa523976839d79d
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-05-30 10:06:19 -0300

    Create repostas.py

---
+++ repostas.py
@@ -0,0 +1,15 @@
+from primeiropysus import *
+
+print(qtd_total_2019())
+
+print(por_municipio_2019())
+
+print(por_estado())
+
+print(por_mes())
+
+print(por_faixa_etaria())
+
+print(por_sexo())
+
+print(por_cor())
